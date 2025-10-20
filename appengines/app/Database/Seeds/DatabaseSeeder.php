66e43af9e4f7e91521920a7f8bc1e279add0c878
Muhammad Rezky Aulia Saputra <rezkyaulia907@gmail.com>
2025-10-20 18:26:01 +0800

    fix: fixing migration finished

--- appengines/app/Database/Seeds/DatabaseSeeder.php
+++ appengines/app/Database/Seeds/DatabaseSeeder.php
@@ -12,26 +12,25 @@ class DatabaseSeeder extends Seeder
     $this->call('RolesSeeder');
     $this->call('UsersSeeder');
 
-    // 2. Category dan Folder
-    $this->call('CategoriesSeeder');
-    $this->call('FolderSeeder');
-    $this->call('FilesSeeder');
-    $this->call('FolderLinksSeeder');
+    // // 2. Category dan Folder
+    // $this->call('CategoriesSeeder');
+    // $this->call('FolderSeeder');
+    // $this->call('FilesSeeder');
+    // $this->call('FolderLinksSeeder');
 
     // 3. Menu dan Otoritas
     $this->call('MenusSeeder');
-    $this->call('OtoritasSeeder');
-    $this->call('OtoritasFileSeeder');
-    $this->call('OtoritasFolderSeeder');
-
+    // $this->call('OtoritasFileSeeder');
+    // $this->call('OtoritasFolderSeeder');
+    
     // 4. Data tanpa FK
     $this->call('KonfigurasiSeeder');
-
-    $this->call('PersonelSeeder');
-    $this->call('PagesSeeder');
-
+    
+    // $this->call('PersonelSeeder');
+    
     // 8. Password Reset
-    $this->call('PasswordResetsSeeder');
-    $this->call('DokumenSeeder');
+    // $this->call('PasswordResetsSeeder');
+    // $this->call('DokumenSeeder');
+    $this->call('OtoritasSeeder');
   }
 }
