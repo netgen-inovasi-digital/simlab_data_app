89185adbf15e84d12058c4a6d05ff2a2c40fc123
Muhammad Rezky Aulia Saputra <rezkyaulia907@gmail.com>
2025-10-27 16:30:50 +0800

    refactor: migration

--- appengines/app/Database/Seeds/RolesSeeder.php
+++ appengines/app/Database/Seeds/RolesSeeder.php
@@ -16,9 +16,11 @@ class RolesSeeder extends Seeder
 
     // Data roles sesuai kolom yang ada
     $data = [
-      ['id_role' => 1, 'nama_role' => 'Admin', 'grup' => 'admin', 'status_role' => 1],
-      ['id_role' => 2, 'nama_role' => 'User', 'grup' => 'author', 'status_role' => 1],
-      ['id_role' => 8, 'nama_role' => 'Super Admin', 'grup' => 'admin', 'status_role' => 1],
+      ['id_role' => 1, 'nama_role' => 'Admin', 'status_role' => 1],
+      ['id_role' => 2, 'nama_role' => 'Kepala Lab', 'status_role' => 1],
+      ['id_role' => 8, 'nama_role' => 'Super Admin',  'status_role' => 1],
+      ['id_role' => 9, 'nama_role' => 'Asesor',  'status_role' => 1],
+      ['id_role' => 10, 'nama_role' => 'Tim Akreditasi', 'status_role' => 1],
     ];
 
     // Insert batch
